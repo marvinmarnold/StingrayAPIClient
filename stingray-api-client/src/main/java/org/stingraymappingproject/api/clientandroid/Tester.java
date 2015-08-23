fad104df76c1ca70ea71e58585494da3e36b9c7d
Marvin Arnold <marvinmarnold@gmail.com>
2015-08-23 18:26:01 -0500

    Library accessible over internet

---
+++ stingray-api-client/src/main/java/org/stingraymappingproject/api/clientandroid/Tester.java
@@ -0,0 +1,10 @@
+package org.stingraymappingproject.api.clientandroid;
+
+/**
+ * Created by Marvin Arnold on 23/08/15.
+ */
+public class Tester {
+    public String test() {
+        return "this is not a test";
+    }
+}
